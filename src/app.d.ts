4965453e079b8d97810072e612b228d2ffcd3498
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-07-22 12:42:16 +0700

    types: update `app.d.ts` to add `HeroPreview`
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- src/app.d.ts
+++ src/app.d.ts
@@ -9,4 +9,13 @@ declare global {
 	}
 }
 
-export {};
+export interface HeroPreview {
+  id: number
+  name: string
+  name_loc: string
+  name_english_loc: string
+  primary_attr: number
+  complexity: number
+}
+
+export { HeroPreview };
