5d87140d4c3b0aabab07cc0ff155341a69981706
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-07-22 12:42:48 +0700

    tailwind: update config to add `screens` and `fontFamily` theme
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      reaver: ["Reaver", "serif"],
+      radiance: ["Radiance", '"Noto Sans"', '"sans-serif"']
+    },
+    screens: {
+      "sm": "320px",
+      "md": "640px",
+      "lg": "768px",
+      "2lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px"
+    },
     extend: {},
   },
   plugins: [],
