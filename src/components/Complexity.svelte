45da1af2f55bc3a4e7bd7f40ea7f2a701095bcec
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-07-22 15:01:23 +0700

    components: add `<Complexity/>` and `<HeroBox/>` components
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

---
+++ src/components/Complexity.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+</script>
+
+<div class="diamond group-hover:hover"/>
+
+<style>
+  .diamond {
+    width: 0;
+    height: 0;
+    border: 10px solid transparent;
+    border-bottom-color: white;
+    position: relative;
+    top: -10px;
+  }
+
+  .diamond.group-hover\:hover {
+    width: 0;
+    height: 0;
+    border: 10px solid transparent;
+    border-bottom-color: white;
+    position: relative;
+    top: -9.1px;
+  }
+
+  .diamond:after {
+    content: '';
+    position: absolute;
+    left: -10px;
+    top: 10px;
+    width: 0;
+    height: 0;
+    border: 10px solid transparent;
+    border-top-color: white;
+  }
+
+  .diamond:after.group-hover\:hover {
+    content: '';
+    position: absolute;
+    left: -10px;
+    top: 9.1px;
+    width: 0;
+    height: 0;
+    border: 10px solid transparent;
+    border-top-color: white;
+  }
+</style>
---
+++ src/components/HeroBox.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { FILTER } from "$lib";
+	import type { HeroPreview } from "$types";
+	import { onMount } from "svelte";
+	import Complexity from "./Complexity.svelte";
+
+
+  export let hero: HeroPreview;
+  export let index: number;
+  let loaded: boolean = false;
+
+  const getHeroThumbnail = (name: string) => {
+    return `https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${name.replace("npc_dota_hero_", "")}.png`;
+  }
+
+
+  onMount(() => {
+    loaded = true;
+  })
+
+</script>
+
+
+<a
+  href="/characters/{hero.id}"
+  class="group relative w-[150px] h-[80px] xl:w-[256px] xl:h-[144px] hover:z-50 transition {!loaded? `opacity-0 scale-125` : 'opacity-100'} duration-300 scale-100 hover:scale-125 rounded-sm bg-no-repeat bg-cover overflow-hidden"
+  style="background-image: url('{getHeroThumbnail(hero.name)}');
+        box-shadow: -7px 7px 10px black;
+        --index: {index};"
+  draggable="false"
+>
+  <div class="flex space-x-1 absolute top-1 right-2">
+    {#each Array(hero.complexity) as _, i}
+      <div
+        class="transition group-hover:translate-y-0 -translate-y-8 duration-300"
+        style="transition-delay: {(i + 1) * 8}0ms;
+              filter: drop-shadow(-1px 1px 1px black);"
+      >
+        <Complexity/>
+      </div>
+    {/each}
+  </div>
+  <div class="flex items-center absolute bottom-1 group-hover:translate-y-0 group-hover:opacity-100 group-hover:scale-100 translate-y-8 opacity-0 scale-70 transition duration-300 px-3 space-x-2">
+    <img src="{FILTER[hero.primary_attr].attr_hero}" alt="" width="40" height="40">
+    <div class="text-xl uppercase select-none font-reaver font-semibold">{hero.name_english_loc}</div>
+  </div>
+  <div
+    class="absolute bottom-0 w-[256px] h-[70px] -z-50 transition duration-300 group-hover:opacity-100 opacity-0"
+    style="background: linear-gradient(rgba(0, 0, 0, 0) 20%, rgba(0, 0, 0, .7) 60%, rgba(0, 0, 0, .9) 80%);"
+  />
+</a>
+
+
+<style>
+  .transition-delay {
+    transition-delay: calc((var(--index) + 1) * 80ms);
+  }
+</style>
