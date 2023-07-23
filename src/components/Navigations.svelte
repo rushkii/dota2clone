d7a8e5ac04b78d268d6ff361f08686c204fb5c3d
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-07-23 13:21:20 +0700

    components: remove animation on Dota 2's logo and add anchor tag to navigate to the base endpoint
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- src/components/Navigations.svelte
+++ src/components/Navigations.svelte
@@ -1,20 +1,19 @@
 <div class="absolute px-5 lg:px-6 xl:px-7 py-4 xl:py-5">
   <div>
-    <div class="transition-opacity cursor-pointer relative w-max">
+    <a href="/" class="transition-opacity cursor-pointer relative w-max">
       <img
         id="glow-logo"
         src="/images/dota2_logo.png" alt="logo"
         draggable="false"
         class="w-[150px] lg:w-[180px] xl:w-[200px]"
       >
-    </div>
+    </a>
   </div>
 </div>
 
 
 <style>
   #glow-logo {
-    animation: neon-pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
     opacity: .8;
     filter: brightness(50%) sepia(100) saturate(100) hue-rotate(25deg) drop-shadow(0 0 0.6rem rgba(202, 70, 0, .8));
   }
@@ -22,12 +21,5 @@
   #glow-logo:hover {
     opacity: 1;
     filter: brightness(50%) sepia(100) saturate(100) hue-rotate(25deg) drop-shadow(0 0 0.6rem rgba(202, 70, 0, 1));
-  }
-
-  @keyframes neon-pulse {
-    50% {
-      opacity: 1;
-      filter: brightness(50%) sepia(100) saturate(100) hue-rotate(25deg) drop-shadow(0 0 0.6rem rgba(202, 70, 0, 1));
-    }
   }
 </style>
