6e46f2687c26c4ce9966c0c036d6527bd04c62d4
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-07-25 08:23:45 +0700

    lib: add `attr_name` for the `FILTER` constant data
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,19 +1,23 @@
 export const FILTER = [
   {
     attr: "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/herogrid/filter-str-active.png",
-    attr_hero: "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_strength.png"
+    attr_hero: "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_strength.png",
+    attr_name: "Strength"
   },
   {
     attr: "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/herogrid/filter-agi-active.png",
-    attr_hero: "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_agility.png"
+    attr_hero: "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_agility.png",
+    attr_name: "Agility"
   },
   {
     attr: "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/herogrid/filter-int-active.png",
-    attr_hero: "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_intelligence.png"
+    attr_hero: "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_intelligence.png",
+    attr_name: "Intelligence"
   },
   {
     attr: "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/herogrid/filter-uni-active.png",
-    attr_hero: "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_universal.png"
+    attr_hero: "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_universal.png",
+    attr_name: "Universal"
   }
 ]
 
